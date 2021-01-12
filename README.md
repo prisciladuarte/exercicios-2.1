68ab540d29d8e1c53d19ab5213ca464b7fee0f4a
Mabia <49458473+prisciladuarte@users.noreply.github.com>
2021-01-12 16:18:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicios-2.1
+ 
---
+++ src/Program.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double raio, area, pi = 3.14159;
+		
+		raio = sc.nextDouble();
+		
+		area = pi * raio * raio;
+		
+		System.out.printf("A =%.4f%n", area);
+		
+		sc.close();
+	}
+
+}
